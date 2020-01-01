07a7640b30dede6aef9c24b44b358383c235715d
github username <github email address>
2019-12-31 20:50:33 -0800

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+class Artist
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def songs
+        # binding.pry
+        Song.all.select { |song| song.artist == self} 
+    end
+
+    def new_song(name, genre)
+        Song.new(name, self, genre)
+    end
+
+    def genres 
+        
+    end
+
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,25 @@
+class Genre
+
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(name)
+        @name = name 
+        @@all << self
+    end
+
+    def songs
+        Song.all.select { |song| song.genre == self} 
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        Artist.all.select { |artist| artist.genre == self}
+    end
+
+
+end
