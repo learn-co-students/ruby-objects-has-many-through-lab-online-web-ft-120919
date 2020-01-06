a35127c431453912238b16524da257031a8e7b2d
Scott Hoisington <hoyzington@gmail.com>
2020-01-06 21:00:55 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,28 @@
+class Artist
+  
+  attr_accessor :name, :genre
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+  end
+  
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+  
+  def genres
+    songs.collect{|song| song.genre}.uniq
+  end
+  
+end
